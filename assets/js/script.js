26d07dda09f4e357d4bd10bc48fd6ca5630de099
Jeyfred Calderon <jeyfredc@gmail.com>
2020-07-05 23:43:43 -0500

    se añadio funcionalidad al menu y se creo un documento Js

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+document.getElementById("btn-menu").addEventListener("click",function(){
+    alert("Haz hecho click en el boton de menu");
+    document.getElementsByClassName(".menu-navegacion").classList.toggle("show")
+});
